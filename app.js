efe0087c616583d4020843efa5610873f45f7b5c
Nay Hlaing Oo <nayhlaingoo93@gmail.com>
2024-03-27 17:22:24 +0800

    init

---
+++ app.js
@@ -0,0 +1,7 @@
+const sideBar = document.querySelector(".sidebar");
+const button = document.querySelector("#hamburger-menu");
+
+function myfunction() {
+  sideBar.classList.toggle("block");
+  console.log(sideBar);
+}
